4ccec7783ca3905797041547fd84a3b3045a0ba2
FabrizioAtiquipa <fabrizio.atiquipa@tecsup.edu.pe>
2022-01-24 16:06:47 -0500

    Project models

--- croi-backend/core/settings/base.py
+++ croi-backend/core/settings/base.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user.apps.UserConfig',
+    'project.apps.ProjectConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,6 +106,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ croi-backend/project/choices.py
@@ -0,0 +1,6 @@
+documents = (
+    ('P', 'PROJECT'),
+    ('A', 'ACTION'),
+    ('I', 'INVESTMENT'),
+    ('B', 'BOND'),
+)
