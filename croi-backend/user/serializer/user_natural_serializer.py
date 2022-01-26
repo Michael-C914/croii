11f0870827eb0cdd2511bf73f62d6aaffc03798b
angel samanez <angel.samanez@tecsup.edu.pe>
2022-01-26 09:20:06 -0500

    usuario natural datos especificos

--- croi-backend/user/serializer/user_natural_serializer.py
+++ croi-backend/user/serializer/user_natural_serializer.py
@@ -2,11 +2,27 @@
 from rest_framework import serializers
 
 # Models
-from user.models import UserNatural
+from user.models import CustomUser, UserNatural
+from user.serializer.custom_user_serializer import CustomUserSerializer
 
 
 class UserNaturalSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer()
+
     class Meta:
         model = UserNatural
-        fields = '__all__'
+        fields = [
+            'id',
+            'user',
+            'special_user',
+            'DNI',
+            'first_name',
+            'last_name',
+        ]
         read_only_fields = ['special_user']
+
+    def create(self, validated_data):
+        custom_user = validated_data.pop('user')
+        user = CustomUser.objects.create(is_natural=True, **custom_user)
+        user_natural = UserNatural.objects.create(user=user, **validated_data)
+        return user_natural
