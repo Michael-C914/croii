42326f3054431ca7fe5a95a4af650fd4308e612b
Ruben Rodrigo <rodrigohde905@gmail.com>
2022-01-26 16:48:12 -0500

    Update: Update NextAuthConfig

--- croi-frontend/src/pages/api/auth/[...nextauth].js
+++ croi-frontend/src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,4 @@
 import NextAuth from 'next-auth'
-import axios from 'axios'
 import jwt from 'jsonwebtoken';
 import { loginApp } from '../../../services/rest/ApiLogin';
 
@@ -55,10 +54,9 @@ export default NextAuth({
     /**
      * 
      * @param {object} token Información del Token de Autorización del lado del Cliente, disponible para agregar nuevos atributos
-     * @param {object} user Información devuelta por la opción de autenticación (Azure o Credentials)
-     * @param {object} account Información de la cuenta para la autenticación (Azure o Credentials)
+     * @param {object} user Información devuelta por la opción de autenticación (Credentials)
+     * @param {object} account Información de la cuenta para la autenticación (Credentials)
      * La función identifica la opción de Autenticación elegida por el usuario
-     * Azure: Recibe el token de autenticación provisto por Azure y lo envia al sistema backend para recibir un token valido dentro del sistema y lo incluye en el token del lado del cliente.
      * Credentials: Serializa el token recibido en la Autenticación y lo incluye en el token del lado del cliente.
      * Mas información: https://next-auth.js.org/configuration/callbacks#redirect-callback
      * @returns Token que sera serializado para la autenticación del usuario
@@ -68,7 +66,7 @@ export default NextAuth({
         const tok = {
           accessToken: user.data.access,
           refresh: user.data.refresh,
-          accessTokenExpires: Date.now() + (23 * 60 * 60 * 1000)
+          accessTokenExpires: Date.now() + (300)
         }
         //console.log("Primer Sign In Cred------------------------------------------------------------------------------------------------------------------------");
         console.log(tok);
@@ -96,17 +94,21 @@ export default NextAuth({
     async session(session, token) {
       const dec = jwt.decode(token.accessToken, { complete: true });
       session.accessToken = token.accessToken;
-
-      session.id = dec.payload.user_id;
+      session.user_id = dec.payload.user_id;
       session.email = dec.payload.email;
-      session.first_name = dec.payload.first_name;
-      session.last_name = dec.payload.last_name;
+      session.username = dec.payload.email;
+      session.date_joined = dec.payload.date_joined;
       session.is_staff = dec.payload.is_staff;
+      session.is_active = dec.payload.is_active;
       session.is_superuser = dec.payload.is_superuser;
+      session.is_natural = dec.payload.is_natural;
+      session.is_juridic = dec.payload.is_juridic;
+      session.is_project = dec.payload.is_project;
 
       return session;
     }
   },
+  secret: 'kQYoCbG/wNj9ryrQFp6IStd21Nwd7fOoXrw/hVN4JOc=',
   session: {
     // Use JSON Web Tokens for session instead of database sessions.
     // This option can be used with or without a database for users/accounts.
