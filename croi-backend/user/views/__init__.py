92776d9fb26e056bee0977b28e77a3da7818ecf8
angel samanez <angel.samanez@tecsup.edu.pe>
2022-01-26 10:27:32 -0500

    DNI Y RUC UNICOS

--- croi-backend/user/views/__init__.py
+++ croi-backend/user/views/__init__.py
@@ -1,4 +1,5 @@
 from user.views.custom_user_view import *
 from user.views.session_manager_view import *
 from user.views.user_juridic_view import *
-from user.views.user_natural import *
+from user.views.user_natural_view import *
+from user.views.special_user_view import *
